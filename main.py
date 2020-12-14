bd3b2409ad4e61c1d341114beab46c5485347973
hatem1055 <hatemmostafa31@gmail.com>
2020-12-15 00:49:23 +0200

    adding lopping on players

--- main.py
+++ main.py
@@ -7,28 +7,32 @@ from ability_settings import ability_settings
 def open_pes():
     pyautogui.click(Positions.pes[0],Positions.pes[1])
 
-
+def save_player_and_go_to_the_next_one():
+    more_than_one_press(Keys.DOWN,6)
+    press(Keys.ENTER)
+    press(Keys.DOWN)
 #main
-def main():
+def main(number_of_playres):
     '''
     cant done automatic : injury,
     needs cheeking : age
     '''
     open_pes()
-
-    #navigate to basic settings
-    press(Keys.ENTER)
-    press(Keys.DOWN)
-    press(Keys.ENTER)
-    #basic settings
-    basic_settings()
-    #navigate to abillity settings
-    more_than_one_press(Keys.DOWN,2)
-    press(Keys.ENTER)
-    # ability settings
-    ability_settings()
-
+    for _ in range(0,number_of_playres):
+        #navigate to basic settings
+        press(Keys.ENTER)
+        press(Keys.DOWN)
+        press(Keys.ENTER)
+        #basic settings
+        basic_settings()
+        #navigate to abillity settings
+        more_than_one_press(Keys.DOWN,2)
+        press(Keys.ENTER)
+        # ability settings
+        ability_settings()
+        #save
+        save_player_and_go_to_the_next_one()
 
     
 if __name__ == "__main__":
-    main()
+    main(19)
